1fa7bb9eb22ae5ce640ac362cf888f3c7b0b1805
huanggang <huanggang@diligrp.com>
2020-06-30 09:08:38 +0800

    nacos integration

--- upay-boss/src/main/java/com/diligrp/xtrade/upay/UpayServiceBoostrap.java
+++ upay-boss/src/main/java/com/diligrp/xtrade/upay/UpayServiceBoostrap.java
@@ -7,12 +7,14 @@ import com.diligrp.xtrade.upay.trade.TradeConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @ComponentScan(basePackageClasses = {BossConfiguration.class, TradeConfiguration.class,
         ChannelConfiguration.class, CoreConfiguration.class})
+@EnableDiscoveryClient
 public class UpayServiceBoostrap {
     public static void main(String[] args) {
         SpringApplication.run(UpayServiceBoostrap.class, args);
